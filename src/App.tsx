4da4aa96f911291140ac2385e0621da1776586d9
Roman <R.rybkin94@gmail.com>
2022-11-07 12:04:22 +0400

    add new page

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>Hello GIT</p>
+        <p>ADD NEW page</p>
         <a
            className="App-link"
           href="https://reactjs.org"
